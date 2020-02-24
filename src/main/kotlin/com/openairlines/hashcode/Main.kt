b7dddd4e1a75b852cac331b3f5ac5ea7ff904255
Camille Blondiaux <camille.blondiaux@gmail.com>
2020-02-24 20:04:04 +0100

    prepare changes

--- src/main/kotlin/com/openairlines/hashcode/Main.kt
+++ src/main/kotlin/com/openairlines/hashcode/Main.kt
@@ -1,24 +1,35 @@
 package com.openairlines.hashcode
 
+import org.apache.commons.math3.stat.Frequency
 import java.io.File
 import java.lang.Integer.min
+import java.time.LocalDateTime
 
 fun main() {
+    var startMs = System.currentTimeMillis()
+    println(LocalDateTime.now())
     val files = File("inputs").listFiles()?.asList()
     files?.sortedBy { it.name }
-    files?.forEach {
-        println(it.name)
-        processFile(it)
-    }
+    val score = files?.filter { it.name != "d.txt" }
+            ?.map {
+                println(it.name)
+                processFile(it)
+            }?.sum()!!
+    if (score > 24952686)
+        println("NEW SCORE")
+    var endMs = System.currentTimeMillis()
+    println(LocalDateTime.now().toString() + "(${(endMs - startMs) / 1000}s)")
+    println("Total $score")
 }
 
-fun processFile(file: File) {
+fun processFile(file: File): Int {
     // Read input
     val lines = file.readLines()
     val totalNumBooks = lines[0].split(" ")[0].toInt()
     val totalDays = lines[0].split(" ")[2].toInt()
     val allBookScores = lines[1].split(" ").map { it.toInt() }
     val totalBookScores = allBookScores.sum()
+    val uniqueBookScore = if (allBookScores.distinct().count() == 1) allBookScores.first() else null
 
     val libraries = emptyList<Library>().toMutableList()
     for ((idLib, i) in (2 until lines.size - 1 step 2).withIndex()) {
@@ -29,6 +40,18 @@ fun processFile(file: File) {
         libraries.add(Library(idLib, elements[1].toInt(), elements[2].toInt(), books.sortedByDescending { it.score }.toMutableList()))
     }
     val totalLibs = libraries.size
+
+    val booksFreq = Frequency()
+    allBookScores.forEach { booksFreq.addValue(it) }
+    println("Books score")
+    if (uniqueBookScore != null)
+        println("Unique: $uniqueBookScore")
+    else
+        println("min: ${allBookScores.min()}" +
+                ", max: ${allBookScores.max()}" +
+                ", avg: ${allBookScores.average()}" +
+                ", med: ${allBookScores[allBookScores.size / 2]}" +
+                ", mode: ${booksFreq.mode} (*${booksFreq.getCount(booksFreq.mode.firstOrNull())})")
 
     // Start optimizing...
     val outputFileLines = listOf<String>().toMutableList()
@@ -42,18 +65,18 @@ fun processFile(file: File) {
         libraries.removeIf {
             it.signupTime >= remainingDays ||
                     scannedBooks.containsAll(it.books.map { book -> book.id }) ||
-                    it.numberOfNewBookUntil(remainingDays, scannedBooks) <= 0
+                    numberOfNewBookUntil(it, remainingDays, scannedBooks) <= 0
         }
-        libraries.sortByDescending { it.scoreForNewBookUntil(remainingDays, scannedBooks) / it.signupTime }
+        libraries.sortByDescending { scoreForNewBookUntil(it, remainingDays, scannedBooks) / it.signupTime }
         libraries.firstOrNull()?.let { library ->
             libraries.remove(library)
-            val newBooks = library.newBookUntil(remainingDays, scannedBooks)
+            val newBooks = newBookUntil(library, remainingDays, scannedBooks)
             if (newBooks.isNotEmpty()) {
                 // Just some indicators...
                 sentScore += newBooks.sumBy { it.score }
                 sentBooks += newBooks.size
                 // Keep track of already scanned books
-                scannedBooks.addAll(newBooks.map { it.id })
+                //scannedBooks.addAll(newBooks.map { it.id })
                 // Yeah, one more library applied, go to next lib
                 nbLibApplied++
                 remainingDays -= library.signupTime
@@ -74,37 +97,37 @@ fun processFile(file: File) {
     }
 
     println("${outPutFile.name}: totalDays: $totalDays, score: $sentScore/$totalBookScores, books: $sentBooks/$totalNumBooks, libs: $nbLibApplied/$totalLibs")
+    return sentScore
+}
 
+
+fun scoreForNewBookUntil(library: Library, deadline: Int, alreadyScanBooks: Set<Int>): Int {
+    val numOfNewBooks = numberOfNewBookUntil(library, deadline, alreadyScanBooks)
+    return if (numOfNewBooks <= 0)
+        0
+    else
+        library.books.filter { !alreadyScanBooks.contains(it.id) }.subList(0, numOfNewBooks).sumBy { it.score }
+
+}
+
+fun numberOfNewBookUntil(library: Library, deadline: Int, alreadyScanBooks: Set<Int>): Int {
+    return if (deadline <= library.signupTime)
+        0
+    else
+        min((deadline - library.signupTime) * library.shipPerDay,
+                library.books.filter { !alreadyScanBooks.contains(it.id) }.size)
+}
+
+fun newBookUntil(library: Library, deadline: Int, alreadyScanBooks: Set<Int>): List<Book> {
+    val newBooks = library.books.filter { !alreadyScanBooks.contains(it.id) }
+    return if (deadline <= library.signupTime)
+        emptyList()
+    else if ((deadline - library.signupTime) * library.shipPerDay >= newBooks.size)
+        newBooks else
+        newBooks.subList(0, (deadline - library.signupTime) * library.shipPerDay)
 }
 
 
 data class Book(val id: Int, val score: Int)
 
-data class Library(val id: Int, val signupTime: Int, val shipPerDay: Int, val books: MutableList<Book>) {
-
-    fun scoreForNewBookUntil(deadline: Int, alreadyScanBooks: Set<Int>): Int {
-        val numOfNewBooks = numberOfNewBookUntil(deadline, alreadyScanBooks)
-        return if (numOfNewBooks <= 0)
-            0
-        else
-            books.filter { !alreadyScanBooks.contains(it.id) }.subList(0, numOfNewBooks).sumBy { it.score }
-
-    }
-
-    fun numberOfNewBookUntil(deadline: Int, alreadyScanBooks: Set<Int>): Int {
-        return if (deadline <= signupTime)
-            0
-        else
-            min((deadline - signupTime) * shipPerDay, books.filter { !alreadyScanBooks.contains(it.id) }.size)
-    }
-
-    fun newBookUntil(deadline: Int, alreadyScanBooks: Set<Int>): List<Book> {
-        val newBooks = books.filter { !alreadyScanBooks.contains(it.id) }
-        return if (deadline <= signupTime)
-            emptyList()
-        else if ((deadline - signupTime) * shipPerDay >= newBooks.size)
-            newBooks else
-            newBooks.subList(0, (deadline - signupTime) * shipPerDay)
-    }
-
-}
+data class Library(val id: Int, val signupTime: Int, val shipPerDay: Int, val books: MutableList<Book>)
